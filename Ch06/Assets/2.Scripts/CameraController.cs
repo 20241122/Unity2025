8fcb5d30adf4ea9886dd5a744dbb6eb1eeda74ba
20241122 <tyrib1234@gmail.com>
2025-11-03 17:49:21 +0900

    Ch06
    20251103

--- Ch06/Assets/2.Scripts/CameraController.cs
+++ Ch06/Assets/2.Scripts/CameraController.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+
+    public Transform playerTransform;
+    //public GameObject player;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // player = GameObject.Find("cat");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 PlayerPos = this.playerTransform.position;
+        transform.position = new Vector3(transform.position.x,
+            PlayerPos.y + 2,
+            transform.position.z);
     }
+
 }
--- Ch06/Assets/2.Scripts/PlayerController.cs
+++ Ch06/Assets/2.Scripts/PlayerController.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using System.Data;
 using UnityEditor;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rb;
     Animator animator;
-    public float jumpForce = 500f;
+    public float jumpForce = 400f;
     public float walkForce = 30f;
     public float maxWalkSpeed = 10f;
 
@@ -23,8 +23,9 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && rb.velocity.y == 0)
         {
+            animator.SetTrigger("Jump Trigger");
             rb.AddForce(transform.up * jumpForce);
         }
 
@@ -33,16 +34,33 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKey(KeyCode.RightArrow)) key = 1;
 
         float speedX = Mathf.Abs(rb.velocity.x);
-        if(speedX < maxWalkSpeed)
+        if (speedX < maxWalkSpeed)
         {
             rb.AddForce(transform.right * key * walkForce);
         }
 
-        if(key != 0)
+        if (key != 0)
         {
             transform.localScale = new Vector3(key, 1, 1);
         }
 
-        animator.speed = speedX / 2.0f;
+        if (rb.velocity.y == 0)
+        {
+            animator.speed = speedX / 2.0f;
+        }
+        else
+        {
+            animator.speed = 1.0f;
+        }
+
+
+        if (transform.position.y < -10)
+        {
+            SceneManager.LoadScene("Main");
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        SceneManager.LoadScene("ClearScene");
     }
 }
